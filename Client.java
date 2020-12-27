d613edf58e5cc113b1a8d4b69148e5092bce2a7f
Yessmyne <65356102+Yessmyne@users.noreply.github.com>
2020-12-27 15:35:07 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,19 +1,36 @@
+
+package javaapplication2;
+import java.io.*;
 import java.rmi.Naming;
+import java.rmi.*;
+import java.util.Scanner;
+class Client
+{
+public static void main (String [] args)
+{
+ Scanner myObj = new Scanner(System.in);  
+    System.out.println("operation et valeur:");
+
+    String oper = myObj.nextLine(); 
+    String val1 = myObj.nextLine(); 
+    int operation=Integer.parseInt(oper);
+    double val2=Double.parseDouble(val1);
+    
+
+ try {
+    MyInterface cpt= (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server"); 
+
+ if (operation==1) 
+     cpt.crediter(val2);
+ else if (operation ==2) 
+     cpt.debiter(val2);
+ else 
+     System.out.println ("Votre solde = " +cpt.lire_solde());
+ 
 
 
-public class Client {
-
-    public static void main(String[] args) {
-        try {
-            MyInterface service1 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-            service1.crediter(200);
-            service1.debiter(100);
-            System.out.println(service1.lireSolde());
-
-        } catch (Exception e) {
-            System.out.println("Error");
-            System.out.println(e.toString());
-        }
-    }
-
-}
+ }catch (Exception e) {
+    System.out.println("Erreur d'acces a un objet distant");
+    System.out.println(e.toString());
+ }
+ }}
